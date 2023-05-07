687c8c7ab2a3fb44c1cf7919a4d647ce1f768642
Kristijan Jordanovski <jordanovskikr@gmail.com>
2023-05-07 20:05:39 +0200

    Smetka

---
+++ src/Smetka.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+
+public class Smetka {
+public static void main(String[] args) {
+	
+	// jabolki - 50 den kg, krusi - 60 den kg, grozje - 100 den kg
+	
+	double smetka = 0;
+	System.out.println("Vnesi sto si pazarel vo kilogrami");
+	Scanner tastatura = new Scanner (System.in);
+	System.out.println("Jabolki");
+	double jabolki = tastatura.nextDouble();
+	System.out.println("krusi");
+	double krusi = tastatura.nextDouble();
+	System.out.println("grozje");
+	double grozje = tastatura.nextDouble();
+	smetka = jabolki*50+krusi*60+grozje*100;
+	System.out.println("Smetkata iznesuva "+smetka+" denari");
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+}
